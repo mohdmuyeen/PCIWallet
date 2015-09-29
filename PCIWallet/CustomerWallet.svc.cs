4fc98ddf5be958386ebd9b266acd3bb5053c13bd
mohdmuyeen <muyeen.mohd@gmail.com>
2015-09-29 09:08:43 -0700

    Added

---
+++ PCIWallet/CustomerWallet.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace PCIWallet
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CustomerWallet" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select CustomerWallet.svc or CustomerWallet.svc.cs at the Solution Explorer and start debugging.
+    public class CustomerWallet : ICustomerWallet
+    {
+        public List<WalletInfo> RetrieveWallet(string AccountId)
+        {
+            List<WalletInfo> lstWallet = null;
+
+            try
+            {
+                dbPCITokenEntities1 context = new dbPCITokenEntities1();
+                var wallet = context.uspGetWallet(AccountId);
+                var wallets = from e in wallet
+                              select new WalletInfo
+                              {
+                                  AccountID = e.AccountNumber,
+                                  PaymentAccountLastFour = e.BankLastFour,
+                                  RoutingNumber = e.RoutingNumber == null ? string.Empty : e.RoutingNumber,
+                                  ToketID = e.TokenID,
+                                  DisplayName = e.DisplayName
+
+                              };
+                lstWallet = new List<WalletInfo>(wallets.ToList());
+            }
+            catch (Exception)
+            {
+                lstWallet = null;
+            }
+            return lstWallet;
+        }
+    }
+}
---
+++ PCIWallet/ICustomerWallet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace PCIWallet
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICustomerWallet" in both code and config file together.
+    [ServiceContract]
+    public interface ICustomerWallet
+    {
+        [OperationContract]
+        List<WalletInfo> RetrieveWallet(String AccountId);
+    }
+    [DataContract]
+    public class WalletInfo
+    {
+        [DataMember]
+        public String AccountID { get; set; }
+        [DataMember]
+        public String PaymentAccountLastFour { get; set; }
+        [DataMember]
+        public String RoutingNumber { get; set; }
+        [DataMember]
+        public String ToketID { get; set; }
+        [DataMember]
+        public String DisplayName { get; set; }
+    }
+}
