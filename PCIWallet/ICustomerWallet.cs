ed514524ec389a03c0436fc246228ff029a63a6b
mohdmuyeen <muyeen.mohd@gmail.com>
2015-09-30 08:40:27 -0700

    modified code

--- PCIWallet/ICustomerWallet.cs
+++ PCIWallet/ICustomerWallet.cs
@@ -13,21 +13,6 @@ namespace PCIWallet
     {
         [OperationContract]
         List<WalletInfo> RetrieveWallet(String AccountId);
-
-        [OperationContract]
-        List<PaymentHistory> PaymentHistory(String AccountId);
-
-        [OperationContract]
-        String VoidPayment(String AccountId, String TransactionNumber);
-
-        [OperationContract]
-        String ReversePayment(String AccountId, String TransactionNumber);
-
-        [OperationContract]
-        String MakePayment(Payment objPayment);
-
-
-
     }
     [DataContract]
     public class WalletInfo
@@ -42,39 +27,5 @@ namespace PCIWallet
         public String ToketID { get; set; }
         [DataMember]
         public String DisplayName { get; set; }
-    }
-    [DataContract]
-    public class PaymentHistory
-    {
-        [DataMember]
-        public String Accountid { get; set; }
-        [DataMember]
-        public String PaymentDate { get; set; }
-        [DataMember]
-        public String PaymentAmount { get; set; }
-        [DataMember]
-        public Boolean isVoid { get; set; }
-        [DataMember]
-        public Boolean isReversal { get; set; }
-        [DataMember]
-        public String TransactionStatus { get; set; }
-        [DataMember]
-        public String TransactionNumber { get; set; }
-    }
-    [DataContract]
-    public class Payment
-    {
-        [DataMember]
-        public String Accountid { get; set; }
-        [DataMember]
-        public String PaymentAmount { get; set; }
-        [DataMember]
-        public String CreditCard { get; set; }
-        [DataMember]
-        public String BankAccountNumber { get; set; }
-        [DataMember]
-        public String RoutingNumber { get; set; }
-        [DataMember]
-        public String TokenID { get; set; }
     }
 }
